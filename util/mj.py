5159ce34aba80ca92df61bc48d2e8437a6606866
hhj <QingHu@outlook.com>
2018-12-29 17:39:31 +0800

    api first test

---
+++ util/mj.py
@@ -0,0 +1,20 @@
+# _*_coding:utf-8_*_
+import requests
+import json
+from util.get_config import getConfig
+
+
+class MJ:
+
+    def mj_login(self):
+        url = getConfig("MJ", "url")
+        doLogin = getConfig("MJ", "doLogin")
+        username = getConfig("MJ", "username")
+        password = getConfig("MJ", "password")
+        data = {"doLogin": doLogin, "username": username, "password": password}
+        response = requests.post(url, data=data)
+        return response.content
+
+
+if __name__ == "__main__":
+    MJ().mj_login()
