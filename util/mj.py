5e93ef98c169eec84e0c008967120c12e3a076e3
hhj <QingHu@outlook.com>
2019-01-18 18:40:29 +0800

    去去去

--- util/mj.py
+++ util/mj.py
@@ -1,30 +1,68 @@
 # _*_coding:utf-8_*_
 import requests
 import json
+import math
 from util.get_config import getConfig
 
 
-def mj_login(func):
-
-    def logging():
-        url = getConfig("MJ", "url1")
-        doLogin = getConfig("MJ", "doLogin")
-        username = getConfig("MJ", "username")
-        password = getConfig("MJ", "password")
-        data = {"doLogin": doLogin, "username": username, "password": password}
-        response = requests.post(url, data=data)
-        print(response.status_code)
-        return func
-    return logging()
+def login():
+    url = getConfig("MJ", "url1")
+    doLogin = getConfig("MJ", "doLogin")
+    username = getConfig("MJ", "username")
+    password = getConfig("MJ", "password")
+    data = {"doLogin": doLogin, "username": username, "password": password}
+    s = requests.session()
+    res = s.post(url=url, data=data)
+    return s
 
 
-@mj_login
-def mj_addhouser():
+def mj_get_house(houseType):
     url = getConfig("MJ", "url2")
-    data = {"pageNo": 1, "pageSize": 20, "houseRentType": 1}
-    response = requests.post(url, data=data)
-    print(response.status_code)
+    data = {"pageNo": 1, "pageSize": 20, "houseRentType": houseType}
+    rs = login().post(url=url, data=data)
+    house_dump = rs.text
+    house_info = json.loads(house_dump)
+    house_no = math.ceil(house_info.get('records')/20)
+    accept_house = []
+    for a in range(house_no):
+        data = {"pageNo": a+1, "pageSize": 20, "houseRentType": houseType}
+        rs = login().post(url=url, data=data)
+        house_dump = rs.text
+        house_info = json.loads(house_dump)
+        house_list = house_info.get('result')
+        for b in house_list:
+            hid = b['houseType']
+            if hid == 1:
+                accept_house.append(b['houseId'])
+    return accept_house
 
 
-if __name__ == "__main__":
-    mj_addhouser()
+def dict_get(dict, objkey, default):
+    tmp = dict
+    for k, v in tmp.items():
+        if k == objkey:
+            return v
+    else:
+        if type(v).__name__ == 'dict':
+            ret = dict_get(v, objkey, default)
+            if ret is not default:
+                return ret
+    return default
+
+
+def mj_add_paytype():
+    a = mj_get_house(1)
+    b = mj_get_house(2)
+    if len(a) > 0:
+        for c in a:
+            url = getConfig("MJ", "url3")
+            house_id = c
+            data = {"houseId": c}
+            rs = login().post(url=url, data=data)
+            house_dump = rs.text
+            house_id = json.loads(house_dump)
+            d = dict_get(house_id, 'leaseRoomRentTypeList', None)
+            print(d)
+
+
+mj_add_paytype()
